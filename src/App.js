c35366d5a66c164c4484d6eeab3f392deb1cb75b
Konstantin Bogdanov <kbogdanov1@gmail.com>
2017-08-17 22:12:32 +0300

    structure change

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ToDo from './ToDo';
+
+class App extends Component {
+  render() {
+    return (
+      <ToDo />
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ToDo from './ToDo';
+import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ToDo />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+
