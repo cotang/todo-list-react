fe6ffc1956df057688253ce5ce24a60a11d3a910
Konstantin Bogdanov <kbogdanov1@gmail.com>
2017-08-19 01:23:34 +0300

    Remake to Controlled Components

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import './ToDo.css';
 
+
 class ToDo extends Component {
   constructor(){
     super();
     this.state = {
-      list: []
+      list: [],
+      value: '',
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmitToList = this.handleSubmitToList.bind(this);    
   }
-  addToList(){
-    var input = document.querySelector('#new-action');
+
+  handleChange(event) {
     this.setState({
-      list: [...this.state.list, {'title':input.value, 'completed':false}]
-    })
-    input.value='';
+      value: event.target.value,
+    });
   }
+
+  handleSubmitToList(event) {
+    event.preventDefault();
+    this.setState({      
+      list: [...this.state.list, {'title':this.state.value, 'completed':false}],
+      value: '',
+    })
+  }
+
   completeAction(index){
     var newList = this.state.list.slice();
     newList[index].completed = !(this.state.list[index].completed);
@@ -35,8 +47,15 @@ class ToDo extends Component {
       <div className="to-do">
         <h1 className="to-do__title">To-do list</h1>
         <div>
-          <input type="text" name="new-action" id="new-action" placeholder="Write down the new action" />
-          <button onClick={this.addToList.bind(this)}>Add action</button>
+          <input 
+            type="text" 
+            name="new-action" 
+            id="new-action" 
+            placeholder="Write down the new action"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmitToList}>Add action</button>
         </div>
         <ol className="to-do__list" id="to-do__list">
           {this.state.list.map((item, index) =>
